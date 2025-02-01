1d56f5703c9dcdbdf0a410077f77b25daee6145f
Sohail Sheikh <ssohailsheikh92@gmail.com>
2025-02-01 14:55:30 +0530

    lambda with arguments

--- src/LamdaExp.java
+++ src/LamdaExp.java
@@ -4,6 +4,11 @@ interface ImplementingLambda{
 //    public void lambda2();
 }
 
+interface ImplementingLambda2
+{
+    //lambda with arguments
+    public void lambda2(String str);
+}
 public class LamdaExp {
     public static void main(String[] args) {
         ImplementingLambda im ;
@@ -11,5 +16,11 @@ public class LamdaExp {
             System.out.println("implemented lambda expression");
         };
         im.lambda();
+        ImplementingLambda2 im2;
+        im2 =(s) -> {
+            System.out.println("lambda with arguments");
+            System.out.println(s);
+        };
+        im2.lambda2("printed lamemda with arguments");
     }
 }
