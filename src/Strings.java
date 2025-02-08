2bd74e8d152e7229b68a04178c88265c30d693ff
Sohail Sheikh <ssohailsheikh92@gmail.com>
2025-02-08 21:59:13 +0530

    reversing the String

--- src/Strings.java
+++ src/Strings.java
@@ -1,5 +1,17 @@
 public class Strings {
-    public static void main(String[] args) {
-        System.out.println("Starting Strings");
+
+    static String reverse(String s){
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        s=new String(sb);
+        return s;
     }
+
+    public static void main(String[] args) {
+//        System.out.println("Starting Strings");
+        String s="Welcome to gfg";
+        System.out.println(reverse(s));
+
+    }
+
 }
