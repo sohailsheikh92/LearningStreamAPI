001ef6e1ef549d8b55867a9c9bcaecdcbd37ae9d
Sohail Sheikh <ssohailsheikh92@gmail.com>
2025-02-26 11:51:40 +0530

    Learned traversing of a LinkedList

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,2 +1,29 @@
 public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Checking Wroking");
+        Node head=new Node(10);
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+        head.next.next.next=new Node(40);
+        display(head);
+
+    }
+    static void display(Node head){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+}
+
+
+class Node{
+        int data;
+        Node next;
+        Node(int x){
+            data=x;
+            next=null;
+        }
 }
