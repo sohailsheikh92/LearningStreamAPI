404f7e5f936c172a963ee893358a2c31307399ff
Sohail Sheikh <ssohailsheikh92@gmail.com>
2025-03-04 23:42:41 +0530

    learned how to insert at begin

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,40 +1,55 @@
 public class LinkedList {
     public static void main(String[] args) {
-        System.out.println("Checking Wroking");
-        Node head=new Node(10);
-        head.next=new Node(20);
-        head.next.next=new Node(30);
-        head.next.next.next=new Node(40);
-//        display(head);
+        System.out.println("Checking Working");
+
+        // Creating a linked list
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        // Insert at beginning
+        head = insertAtBegin(head, 5);
+
+        // Display the list
+        display(head);
+
+        // Recursive display
         recursiveDisplay(head);
     }
-    static void display(Node head){
-        Node temp=head;
-        while(temp!=null){
-            System.out.print(temp.data+"->");
-            temp=temp.next;
+
+    static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
         }
         System.out.println("null");
     }
 
-    static void recursiveDisplay(Node head){
-        if(head==null){
-            System.out.println("null");
-            return ;
-        }
-        System.out.print(head.data+"->");
-        head=head.next;
-        recursiveDisplay(head);
+    // Insert at the beginning (Static method for main)
+    public static Node insertAtBegin(Node head, int x) {
+        Node newHead = new Node(x);
+        newHead.next = head;
+        return newHead;
+    }
 
+    static void recursiveDisplay(Node head) {
+        if (head == null) {
+            System.out.println("null");
+            return;
+        }
+        System.out.print(head.data + "->");
+        recursiveDisplay(head.next);
     }
 }
 
-
-class Node{
-        int data;
-        Node next;
-        Node(int x){
-            data=x;
-            next=null;
-        }
+// Node class
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
 }
